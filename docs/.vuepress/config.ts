f12d440fa133dcf3430b0474b3112064a584723b
lyq <1661898579@qq.com>
2024-02-01 21:21:10 +0800

    fix 修改关于页面

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,17 +29,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     nav: [
       { text: '首页', link: '/' },
       {
-        text: '前端',
-        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-        // items: [
-        //   // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-        // ],
-      },
-      {
-        text: '页面',
-        link: '/ui/',
-      },
-      {
         text: '技术',
         link: '/technology/',
       },
@@ -48,15 +37,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: '/more/',
       },
       { text: '关于', link: '/about/' },
-      {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
-      },
+      // {
+      //   text: '收藏',
+      //   link: '/pages/beb6c0bd8a66cea6/',
+      // },
       {
         text: '索引',
-        link: '/archives/',
+        link: '/archives/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
-          { text: '分类', link: '/categories/' },
+          { text: '分类', link: '/categories/' }, //以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
           { text: '标签', link: '/tags/' },
           { text: '归档', link: '/archives/' },
         ],

6bf18661fc3fc206b6265d63187aca165458e858
LazuliKao <LazuliKao233@outlook.com>
2024-03-17 17:18:55 +0800

    切换主题

--- docs/.vuepress/components/giscusBox.vue
+++ docs/.vuepress/components/giscusBox.vue
@@ -1,33 +1,89 @@
 <template>
   <div>
-    <script
-      src="https://giscus.app/client.js"
-      data-repo="nbtca/blogs"
-      data-repo-id="R_kgDOKylXpA"
-      data-category="General"
-      data-category-id="DIC_kwDOKylXpM4CbVRo"
-      data-mapping="title"
-      data-strict="0"
-      data-reactions-enabled="1"
-      data-emit-metadata="0"
-      data-input-position="bottom"
-      :data-theme="theme"
-      data-lang="zh-CN"
-      data-loading="lazy"
-      crossorigin="anonymous"
-      async
-    ></script>
+    <div v-if="show">
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="nbtca/blogs"
+        data-repo-id="R_kgDOKylXpA"
+        data-category="General"
+        data-category-id="DIC_kwDOKylXpM4CbVRo"
+        data-mapping="title"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        :data-theme="theme"
+        data-lang="zh-CN"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async
+      ></script>
+    </div>
+    <div v-else class="change-theme">
+      <!-- skelon text -->
+      <p>正在切换主题...</p>
+    </div>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.change-theme {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+}
+</style>
 <script lang="ts">
-// https://github.com/giscus/giscus-component/blob/main/vue/src/lib/types.ts
 export default {
-  props: {
+  data() {
+    return {
+      // https://github.com/giscus/giscus-component/blob/main/vue/src/lib/types.ts
+      theme: this.getCurrentTheme(),
+      show: true,
+    };
+  },
+  watch: {
     theme: {
-      type: String,
-      default: "noborder_gray",
+      //主题变化时重新加载giscus
+      handler: function (val, oldVal) {
+        this.show = false;
+        // 让 giscus 重新加载
+        setTimeout(() => (this.show = true), 1000);
+      },
     },
+  },
+  methods: {
+    getCurrentTheme() {
+      if (document.body.classList.contains("theme-mode-dark")) {
+        return "noborder_dark";
+      } else {
+        return "noborder_light";
+      }
+    },
+    refreshTheme() {
+      //   console.log("className 变动:", document.body.classList);
+      this.theme = this.getCurrentTheme();
+    },
+  },
+  mounted() {
+    new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        // 检查是否是我们关心的变动（属性变动并且是className）
+        if (
+          mutation.type === "attributes" &&
+          mutation.attributeName === "class"
+        ) {
+          // 在控制台打印新的 className 值
+          this.refreshTheme();
+        }
+      });
+    })
+      // 配置 MutationObserver 来监视 document.body
+      .observe(document.body, {
+        attributes: true,
+        attributeOldValue: true,
+        attributeFilter: ["class"],
+      });
   },
 };
 </script>
--- docs/index.md
+++ docs/index.md
@@ -28,11 +28,13 @@ features: # 可选的
     # details: 其他文章
     link: /more/
     imgUrl: /img/more_1.png
-
 # 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
 # postList: detailed
 # simplePostListLength: 10 # 简约版文章列表显示的文章数量，默认10。（仅在postList设置为simple时生效）
 # hideRightBar: true # 是否隐藏右侧边栏
 ---
+
 <vueSnow></vueSnow>
+
+<Vssue title="test" :options="{ locale: 'zh' }" />
 <giscusBox></giscusBox>
